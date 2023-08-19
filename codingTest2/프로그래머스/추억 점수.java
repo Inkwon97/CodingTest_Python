c20c823bca3811d27e1829f28dfaa57e799fc0cd
AzureAD\황인권 <ghkdrnjs180@naver.com>
2023-08-19 10:51:12 +0900

    프로그래머스 추억 점수

---
+++ codingTest2/프로그래머스/추억 점수.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+        public static int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < name.length; i++) {
+            map.put(name[i], yearning[i]);
+        }
+
+        for (int i = 0; i < photo.length; i++) {
+            int score = 0;
+            for (int j = 0; j < photo[i].length; j++) {
+                score += map.getOrDefault(photo[i][j], 0);
+            }
+            answer[i] = score;
+        }
+
+        return answer;
+    }
+}
